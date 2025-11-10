8260f9e7ec728334ed0eeb7f6f20b3c66651f3f8
Nhat Minh <nguyenhuunhatminh426@gmail.com>
2025-11-11 03:15:33 +0700

    done function verify_password

--- Smart_Travelling/BE/app/domain/entities/user_entity.py
+++ Smart_Travelling/BE/app/domain/entities/user_entity.py
@@ -10,13 +10,29 @@ pwd_context = CryptContext(schemes=["bcrypt"])
 # Class để nói đến các behavior liên qua đến người dùng
 class UserEntity(User):
     
-    """
-    Hash mật khẩu người dùng rồi lluw vào hashed_password
-    sau đó cập nhật update_at để biết thời gian thay đổi gần nhất
-    
-    """
+   
     def set_password(self, password: str):
+        """
+        Hash mật khẩu người dùng rồi lluw vào hashed_password
+        sau đó cập nhật update_at để biết thời gian thay đổi gần nhất
+        """
         self.hashed_password = pwd_context.hash(password)
         self.updated_at = datetime.now(timezone.utc)
+        
+        
+        
+    def verify_password(self, password: str) -> bool:
+        """
+        Kiểm tra xem mật khẩu có khớp không
+        cách hoạt động là Passlib.verify() tự đọc salt từ chuỗi hash đã lưu và hash lại input
+        
+        trả về:
+        True: đúng mk
+        False: sai mk
+        """
+        return pwd_context.verify(password, self.hashed_password)
+    
+    
+    
         
     
