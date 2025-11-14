345ff685e49d6fbaded64fb998a0a094ebddb7cb
Nhat Minh <nguyenhuunhatminh426@gmail.com>
2025-11-14 18:29:20 +0700

    viết lại theo dạng hàm kết nối để mỗi lần có reqquest thì mới mở cổng ra

--- Smart_Travelling/BE/app/infrastructure/database/connectdb.py
+++ Smart_Travelling/BE/app/infrastructure/database/connectdb.py
@@ -1,13 +1,13 @@
 import mysql.connector
+from mysql.connector import Error
 
-db = mysql.connector.connect(user='root', password='Minh12345678!', host='localhost', database="travel")
-
-#query
-code = 'create schema `travel`;'
-
-#run
-mycursor = db.cursor()
-mycursor.execute(code)
-
+def get_db():
+    
+    try:
+        connection = mysql.connector.connect( host="localhost", user="root", password="Minh12345678!", database="travel")
+        return connection
+    except Error as e:
+        print("Lỗi kết nối MySQL:", e)
+        return None
 
 
