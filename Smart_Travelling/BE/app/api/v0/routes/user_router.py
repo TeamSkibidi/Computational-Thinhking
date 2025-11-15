5adfd24c921274c36fe0d52dbf1ae7bebd949a9b
Nhat Minh <nguyenhuunhatminh426@gmail.com>
2025-11-15 17:55:26 +0700

    viết router 'cập nhật thông tin emai,phone' router 'lấy thông tin người dùng profile', router 'đổi mật khẩu'

---
+++ Smart_Travelling/BE/app/api/v0/routes/user_router.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter
+from typing import Dict
+
+from app.application.services import user_service
+from app.utils.response_format import success, error
+
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
+
+
+# LẤY PROFILE
+@router.get("/{user_id}")
+def get_profile(user_id: int):
+    user = user_service.get_user_profile(user_id)
+    if user is None:
+        return error("User không tồn tại")
+
+    return success("Lấy thông tin user thành công!", data=user)
+
+
+
+# CẬP NHẬT THÔNG TIN USER
+@router.put("/update-info")
+def update_info(data: Dict):
+    try:
+        user_service.update_user_info(
+            user_id=data["user_id"],
+            data=data
+        )
+        return success("Cập nhật thông tin thành công!")
+    except ValueError as e:
+        return error(str(e))
+
+
+
+# ĐỔI MẬT KHẨU
+@router.put("/change-password")
+def change_password(data: Dict):
+    try:
+        user_service.change_password(
+            user_id=data["user_id"],
+            old_password=data["old_password"],
+            new_password=data["new_password"]
+        )
+        return success("Đổi mật khẩu thành công!")
+    except ValueError as e:
+        return error(str(e))
