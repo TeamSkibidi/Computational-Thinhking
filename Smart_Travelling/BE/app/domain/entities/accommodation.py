196f08aa38a05bb7028e54d2d47c4b784285b1e6
Théng <thangtccy@gmail.com>
2025-11-25 16:27:03 +0700

    Update accommodation.py

--- Smart_Travelling/BE/app/domain/entities/accommodation.py
+++ Smart_Travelling/BE/app/domain/entities/accommodation.py
@@ -1,55 +1,39 @@
 from typing import Optional, List, Any
 from dataclasses import dataclass 
 from pydantic import BaseModel, Field, field_validator
-import re
-from domain.entities.Address import Address
-# class Address(BaseModel):
-#     houseNumber: Optional[str] = None
-#     street: Optional[str] = None
-#     ward: Optional[str] = None
-#     district: Optional[str] = None
-#     city: Optional[str] = None
-#     lat: Optional[float] = Field(None, ge=-90, le=90, description="Latitude between -90 and 90")
-#     lng: Optional[float] = Field(None, ge=-180, le=180, description="Longitude between -180 and 180")
-#     url: Optional[str] = None
-
-# def get_full_address(self) -> str:
-#     parts = [
-#         self.houseNumber,
-#         self.street,
-#         self.ward,
-#         self.district,
-#         self.city
-#     ]
-#     return ", ".join(filter(None, parts))
+from .Address import Address
 
 class Accommodation (BaseModel):
-    name : Optional[str]
-    capacity : Optional[int] 
-    priceVND : Optional[float]
-    summary : Optional[str] = None
+    name : Optional[str] = Field(..., description="Tên chỗ ở")
+    capacity : Optional[int] = Field( None, ge = 1, description="Sức chứa tối đa")
+    priceVND : Optional[float] = Field(None, ge=0, description="Giá ước lượng (VND)")       
+    summary : Optional[str] = None 
     phone : Optional[str] = None
     rating : Optional[float] = Field (None, ge = 0, le = 5, description="Rating from 0 to 5")
     reviewCount: Optional[int] = Field (None, ge=0, description="Number of reviews")
     popularity : Optional[int] = Field(None, ge = 0, description ="Popularity score")
+    category: Optional[str] = "Hotel"
+    tags: List[str] = Field(default_factory=list,description="Danh sách tag liên quan đến địa điểm")
     address: Optional[Address] = None
 
-    def save (self) -> dict :
+    def save(self) -> dict:
         return {
-            "name" : self.name,
-            "capacity" : self.capacity,
-            "priceVND" : self.priceVND,
-            "summary" : self.summary,
-            "phone" : self.phone,
-            "rating" : self.rating,
-            "reviewCount" : self.reviewCount,
-            "populariy" : self.popularity,
-            "address" : self.address
-        }
+        "name": self.name,
+        "capacity": self.capacity,
+        "priceVND": self.priceVND,
+        "summary": self.summary,
+        "phone": self.phone,
+        "rating": self.rating,
+        "reviewCount": self.reviewCount,
+        "popularity": self.popularity,
+        "category": self.category,
+        "tags": self.tags,
+        "address": self.address.model_dump() if self.address else None,
+    }
+
 
     @classmethod
     def from_json(cls, data : dict) -> "Accommodation":
         return cls(**data)
-
 
 
