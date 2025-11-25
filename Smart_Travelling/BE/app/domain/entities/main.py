454e42d6ed930cc457d76705df1977b85cfb33a6
Théng <thangtccy@gmail.com>
2025-11-25 15:49:26 +0700

    Delete Smart_Travelling/BE/app/domain/entities/main.py
    Delete file

--- Smart_Travelling/BE/app/domain/entities/main.py
+++
@@ -1,7 +0,0 @@
-from user_entity import UserEntity
-
-user = UserEntity(username="test", email="t@t.com", hashed_password="")
-user.set_password("123456")
-print(user.hashed_password)  # hash
-print(user.verify_password("123456"))  # True
-print(user.verify_password("sai"))     # False
