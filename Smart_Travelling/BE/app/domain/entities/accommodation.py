26359f6ce6d16137408a37aa53e8c313f6157769
Thang <Thangtccy@gmail.com>
2025-11-26 21:00:25 +0700

    Đổi sáng kế thừa từ place

--- Smart_Travelling/BE/app/domain/entities/accommodation.py
+++ Smart_Travelling/BE/app/domain/entities/accommodation.py
@@ -1,39 +1,17 @@
-from typing import Optional, List, Any
-from dataclasses import dataclass 
-from pydantic import BaseModel, Field, field_validator
-from .Address import Address
+from typing import Optional
+from pydantic import Field
+from .place import Place
 
-class Accommodation (BaseModel):
-    name : Optional[str] = Field(..., description="Tên chỗ ở")
-    capacity : Optional[int] = Field( None, ge = 1, description="Sức chứa tối đa")
-    priceVND : Optional[float] = Field(None, ge=0, description="Giá ước lượng (VND)")       
-    summary : Optional[str] = None 
-    phone : Optional[str] = None
-    rating : Optional[float] = Field (None, ge = 0, le = 5, description="Rating from 0 to 5")
-    reviewCount: Optional[int] = Field (None, ge=0, description="Number of reviews")
-    popularity : Optional[int] = Field(None, ge = 0, description ="Popularity score")
-    category: Optional[str] = "Hotel"
-    tags: List[str] = Field(default_factory=list,description="Danh sách tag liên quan đến địa điểm")
-    address: Optional[Address] = None
+class Accommodation(Place):
+    capacity: Optional[int] = Field(None, ge=1, description="Sức chứa tối đa")
+    category: str = "Hotel" 
+    summary: Optional[str] = None
+    phone: Optional[str] = None
 
     def save(self) -> dict:
-        return {
-        "name": self.name,
-        "capacity": self.capacity,
-        "priceVND": self.priceVND,
-        "summary": self.summary,
-        "phone": self.phone,
-        "rating": self.rating,
-        "reviewCount": self.reviewCount,
-        "popularity": self.popularity,
-        "category": self.category,
-        "tags": self.tags,
-        "address": self.address.model_dump() if self.address else None,
-    }
-
+        data = self.model_dump()
+        return data
 
     @classmethod
-    def from_json(cls, data : dict) -> "Accommodation":
+    def from_json(cls, data: dict) -> "Accommodation":
         return cls(**data)
-
-
