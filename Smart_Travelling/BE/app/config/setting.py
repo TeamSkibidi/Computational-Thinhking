aff10f806bbb56e46f27ac2836795f8cf83de97a
Théng <thangtccy@gmail.com>
2025-11-26 19:05:13 +0700

    Update settings with DB_DSN and default configurations
    Add database connection settings and default values

--- Smart_Travelling/BE/app/config/setting.py
+++ Smart_Travelling/BE/app/config/setting.py
@@ -1,1 +1,8 @@
+import os
+
+DB_DSN = os.getenv("DB_DSN", "postgresql://user:pass@localhost:5432/yourdb")
+IMAGE_BASE_URL = "http://localhost:8000/static/place_images/"
+
+MAX_PLACES_PER_BLOCK_DEFAULT = 3
+MAX_LEG_DISTANCE_KM_DEFAULT = 5.0
 
