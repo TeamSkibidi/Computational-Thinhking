3fd65086f85a05950e6756a475d7077b99268181
KhoiVu22 <vhdkhoi2432@clc.fitus.edu.vn>
2025-11-29 18:47:52 +0700

    Hàm load dữ liệu từ file.csv

--- Smart_Travelling/BE/app/scripts/seed_events.py
+++ Smart_Travelling/BE/app/scripts/seed_events.py
@@ -1,28 +1,27 @@
-import json 
 import asyncio
-from datetime import datetime
 from typing import List
 
-from app.domain.entities.event import Event 
-from app.adapters.repositories.mysql_event_repository import MySQLEventRepository
-from app.scripts.seed_utils import load_events_from_json
+from app.domain.entities.event import Event
+from Smart_Travelling.BE.app.adapters.repositories.event_repository import MySQLEventRepository
+from app.scripts.seed_utils import load_events_from_csv  # 👈 dùng CSV
 
-JSON_PATH = "app/data/events.json"
+CSV_PATH = "DB.events.csv"
 
-async def main ():
-    # 1. Load dữ liệu từ JSON
-    events: List[Event] = load_events_from_json(JSON_PATH)
-    print(f"Đọc được {len(events)} events từ {JSON_PATH}")
+
+async def main():
+    # 1. Load dữ liệu từ CSV
+    events: List[Event] = load_events_from_csv(CSV_PATH)
+    print(f"Đọc được {len(events)} events từ {CSV_PATH}")
 
     if not events:
         print("Không có event nào để seed, dừng.")
-        return 
-    
+        return
+
     # 2. Tạo repo và upsert vào DB
     repo = MySQLEventRepository()
     await repo.upsert_events(events)
     print("✅ Đã seed xong events vào DB.")
 
-if __name__== "__main__":
-        asyncio.run(main())
 
+if __name__ == "__main__":
+    asyncio.run(main())
